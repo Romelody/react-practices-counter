4b13206b007df9a108ee0a9ae4f933b69cbe0d61
Melody <romelody.calde@gmail.com>
2020-02-03 20:17:39 -0300

    first attempt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Contador/>
     </div>
   );
+}
+
+class Contador extends Component {
+  constructor (props){
+    super(props);
+
+    this.state={
+      contador: 0
+    };
+  }
+
+  aumentar = () => {
+    this.setState(
+      {
+        contador: this.state.contador + 1
+      }
+    )
+  }
+
+  reiniciar = () => {
+    this.setState(
+      {
+      contador: 0
+      }
+    )
+  }
+
+  render(){
+    return (<div>
+      <p>{this.state.contador}</p>
+      <button onClick={this.aumentar}>Aumentar</button>
+      <button onClick={this.reiniciar}>Reiniciar</button>
+    </div>
+      )
+  }
 }
 
 export default App;
